8ff02b4b7f8c6dcf51d60943ba400b9a44b3eb0d
HasinAbrer <hasinjunior12@gmail.com>
2025-08-10 18:59:21 +0600

    added partial event handler to home page add money button

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('home added')
+document.getElementById('btn-add-money').addEventListener('click', function(event) {
+    event.preventDefault()
+    
+    const addMoneyInput = document.getElementById('input-add-money').value
+    const pinNumberInput = document.getElementById('input-pin-number').value
+})
+
